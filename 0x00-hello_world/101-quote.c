0d840d2d61866515e274ad1bb2fae1ea6c6a1291
Yassine H <68260654+yassine-hamadou@users.noreply.github.com>
2022-04-11 04:41:46 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,7 @@
  */
 int main(void)
 {
-	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	
-	write(2, ch, sizeof(ch)); 
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
